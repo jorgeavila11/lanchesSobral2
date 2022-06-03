a2b2ee4aa9534e4f55575331d6c442638304b7b3
jorge <jorge@DESKTOP-5K8QRG5>
2022-06-03 20:17:38 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesSobral2/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LanchesSobral2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesSobral2.Context
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanches> Lanches { get; set; }
+
+    }
+}
---
+++ LanchesSobral2/Migrations/20220531235537_PopularCategoria.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesSobral2.Migrations
+{
+    public partial class PopularCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO categorias VALUES('Normal','Lanche feito com ingredientes normais')");
+            migrationBuilder.Sql("INSERT INTO categorias VALUES('Natural','Lanche feito com ingredientes integrais e naturais')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM categorias");
+        }
+    }
+}
